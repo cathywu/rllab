4b69b61465c64bd5f794c5f390013526f5cffb9e
Cathy Wu <cathywu@eecs.berkeley.edu>
2017-04-11 13:57:47 -0700

    Algorithm comparison for Walker2d-v1, based on published QProp parameters (TRPO, VPG)

--- examples/trpo_gym_tf.py
+++ examples/trpo_gym_tf.py
@@ -1,4 +1,7 @@
+import tensorflow as tf
+
 from sandbox.rocky.tf.algos.trpo import TRPO
+from sandbox.rocky.tf.algos.vpg import VPG
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.envs.gym_env import GymEnv
 from rllab.envs.normalized_env import normalize
@@ -8,34 +11,66 @@ from sandbox.rocky.tf.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from sandbox.rocky.tf.envs.base import TfEnv
 from rllab.misc.instrument import stub, run_experiment_lite
 
+# algo = "VPG"
+algo = "TRPO"
+
+exp_prefix = "Walker2d-comparison"
+
 stub(globals())
 
-env = TfEnv(normalize(GymEnv("Walker2d-v1")))
+env = TfEnv(normalize(GymEnv("Walker2d-v1", force_reset=True),
+                      normalize_obs=False))
 # env = TfEnv(normalize(GymEnv("Pendulum-v0")))
 
 policy = GaussianMLPPolicy(
     name="policy",
     env_spec=env.spec,
     # The neural network policy should have two hidden layers, each with 32 hidden units.
-    hidden_sizes=(32, 32)
+    hidden_sizes=(100, 50, 25),
+    hidden_nonlinearity=tf.nn.tanh,
 )
 
 baseline = LinearFeatureBaseline(env_spec=env.spec)
 
-algo = TRPO(
-    env=env,
-    policy=policy,
-    baseline=baseline,
-    batch_size=4000,
-    max_path_length=env.horizon,
-    n_itr=40,
-    discount=0.99,
-    step_size=0.01,
-    # optimizer=ConjugateGradientOptimizer(hvp_approach=FiniteDifferenceHvp(base_eps=1e-5))
+# Parameters from https://github.com/shaneshixiang/rllabplusplus/blob/master/
+# sandbox/rocky/tf/launchers/launcher_utils.py
+# Parameter matching from https://github.com/shaneshixiang/rllabplusplus/blob/
+# master/sandbox/rocky/tf/launchers/launcher_stub_utils.py
+if algo == 'TRPO':
+    algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=5000,
+        max_path_length=env.horizon,
+        n_itr=1000,
+        discount=0.99,
+        step_size=0.01,
+        sample_backups=0,
+        # optimizer=ConjugateGradientOptimizer(hvp_approach=FiniteDifferenceHvp(base_eps=1e-5))
+        gae_lambda=0.97,
 
-)
+    )
+elif algo == 'VPG':
+    algo = VPG(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=5000,
+        max_path_length=env.horizon,
+        n_itr=1000,
+        discount=0.99,
+        optimizer_args=dict(
+            tf_optimizer_args=dict(
+                learning_rate=0.001,
+            )
+        ),
+        gae_lambda=0.97,
+    )
+
 run_experiment_lite(
     algo.train(),
     n_parallel=4,
     seed=1,
+    exp_prefix=exp_prefix,
 )
