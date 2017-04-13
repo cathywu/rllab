Fix dimensionality issues that weren't caught before due to automatic broadcasting

sandbox/rocky/tf/algos/npo_action.py
@@ -82,7 +82,7 @@ class NPOAction(BatchPolopt):
             dist_info_vars = self.policy.dist_info_sym(obs_var,
                                                        state_info_varss[k])
             # TODO(cathywu) split into k 1-by-1 Diagonal covariance matrix
-            kls[k] = dist1.kl_sym(old_dist_info_varss[k], dist_info_vars)
+            kls[k] = dist1.kl_sym(old_dist_info_varss[k], dist_info_vars, idx=k)
             print("CATHYWU lrs", action_vars[k], old_dist_info_varss[k], dist_info_vars)
             lrs[k] = dist1.likelihood_ratio_sym(action_vars[k],
                                               old_dist_info_varss[k],
@@ -90,9 +90,13 @@ class NPOAction(BatchPolopt):
 
         # FIXME(cathywu) incomplete
         k = 0
+        # FIXME(cathywu) review TRPO to see how to properly compute kl term
         mean_kl = tf.reduce_mean(kls[k])
         # TODO(cathywu) product between adv and ratio of likelihoods
-        surr_loss = - tf.reduce_mean(lrs[k] * advantage_vars[k])
+        # surr_loss = - tf.reduce_mean(lr * advantage_var)
+
+        surr_loss = - tf.reduce_mean(tf.add_n([lrs[k] * advantage_vars[k]
+                        for k in range(self.nactions)]))
 
         input_list = ext.flatten_list([
                          obs_var,
@@ -108,6 +112,15 @@ class NPOAction(BatchPolopt):
             inputs=input_list,
             constraint_name="mean_kl"
         )
+
+        # TODO(cathywu) remove
+        self.lrs = lrs
+        self.advantage_vars = advantage_vars
+        self.input_list = input_list
+        self.dist1 = dist1
+        self.action_vars = action_vars
+        self.mean_kl = mean_kl
+
         return dict()
 
     @overrides
@@ -123,9 +136,62 @@ class NPOAction(BatchPolopt):
                                                       expand_dims=True)
                                            for k in
                           self.policy.distribution.dist_info_keys])
-        # print("CATHYWU state info list", state_info_list)
-        # print("CATHYWU dist info list", dist_info_list)
+        print("CATHYWU state info list", state_info_list)
+        print("CATHYWU dist info list", dist_info_list)
         all_input_values += tuple(state_info_list) + tuple(dist_info_list)
+
+        # TODO(cathywu) debug
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.advantage_vars)
+        adv = temp(*all_input_values)
+        print("CATHYWU adv", temp(*all_input_values))
+
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.lrs)
+        lrs = temp(*all_input_values)
+        print("CATHYWU lrs", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.logli_new)
+        logli_new = temp(*all_input_values)
+        print("CATHYWU logli_new", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.logli_old)
+        logli_old = temp(*all_input_values)
+        print("CATHYWU logli_old", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.new_dist_info_vars[
+                                                 "mean"])
+        logli_new_mean = temp(*all_input_values)
+        print("CATHYWU new means", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.new_dist_info_vars[
+                                                 "log_std"])
+        logli_new_log_std = temp(*all_input_values)
+        print("CATHYWU new log_std", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.old_dist_info_vars[
+                                                 "mean"])
+        logli_old_means = temp(*all_input_values)
+        print("CATHYWU old means", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.dist1.old_dist_info_vars[
+                                                 "log_std"])
+        logli_old_log_std = temp(*all_input_values)
+        print("CATHYWU old log_stds", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.action_vars)
+        action_vars = temp(*all_input_values)
+        print("CATHYWU action_vars", temp(*all_input_values))
+        temp = tensor_utils.compile_function(self.input_list,
+                                             self.mean_kl)
+        mean_kl = temp(*all_input_values)
+        print("CATHYWU mean_kl", temp(*all_input_values))
+        # FIXME(cathywu) first 3 actions have large loss and large lrs
+        print("CATHYWU loss terms", [sum(lrs[k] * adv[k]) for k in range(
+            self.nactions)])
+        # import ipdb
+        # ipdb.set_trace()
+
         logger.log("Computing loss before")
         # print("CATHYWU just before error", all_input_values)
         # print("CATHYWU sizes", [x.shape for x in all_input_values])

sandbox/rocky/tf/distributions/diagonal_gaussian.py
@@ -39,11 +39,19 @@ class DiagonalGaussian(Distribution):
         # return TT.sum(
         #     numerator / denominator + TT.log(new_std) - TT.log(old_std ), axis=-1)
 
-    def kl_sym(self, old_dist_info_vars, new_dist_info_vars):
-        old_means = old_dist_info_vars["mean"]
-        old_log_stds = old_dist_info_vars["log_std"]
-        new_means = new_dist_info_vars["mean"]
-        new_log_stds = new_dist_info_vars["log_std"]
+    def kl_sym(self, old_dist_info_vars, new_dist_info_vars, idx=None):
+        if idx is not None:
+            old_means = tf.expand_dims(old_dist_info_vars["mean"], axis=1)
+            old_log_stds = tf.expand_dims(old_dist_info_vars["log_std"], axis=1)
+            new_means = tf.expand_dims(new_dist_info_vars["mean"][:, idx],
+                                       axis=1)
+            new_log_stds = tf.expand_dims(new_dist_info_vars["log_std"][:,
+                                          idx], axis=1)
+        else:
+            old_means = old_dist_info_vars["mean"]
+            old_log_stds = old_dist_info_vars["log_std"]
+            new_means = new_dist_info_vars["mean"]
+            new_log_stds = new_dist_info_vars["log_std"]
         """
         Compute the KL divergence of two multivariate Gaussian distribution with
         diagonal covariance matrices
@@ -55,6 +63,7 @@ class DiagonalGaussian(Distribution):
         # formula:
         # { (\mu_1 - \mu_2)^2 + \sigma_1^2 - \sigma_2^2 } / (2\sigma_2^2) +
         # ln(\sigma_2/\sigma_1)
+        # TODO(cathywu) Why is there no dimensionality problem here?
         numerator = tf.square(old_means - new_means) + \
                     tf.square(old_std) - tf.square(new_std)
         denominator = 2 * tf.square(new_std) + 1e-8
@@ -63,17 +72,27 @@ class DiagonalGaussian(Distribution):
 
     def likelihood_ratio_sym(self, x_var, old_dist_info_vars,
                              new_dist_info_vars, idx=None):
+        self.new_dist_info_vars = new_dist_info_vars
+        self.old_dist_info_vars = old_dist_info_vars
         logli_new = self.log_likelihood_sym(x_var, new_dist_info_vars, idx=idx)
         logli_old = self.log_likelihood_sym(x_var, old_dist_info_vars)
+        self.logli_new = logli_new
+        self.logli_old = logli_old
         return tf.exp(logli_new - logli_old)
 
     def log_likelihood_sym(self, x_var, dist_info_vars, idx=None):
-        means = dist_info_vars["mean"]
-        log_stds = dist_info_vars["log_std"]
-        if idx is None:
-            zs = (x_var - means) / tf.exp(log_stds)
+        if idx is not None:
+            x_var = tf.expand_dims(x_var, axis=1)
+            means = tf.expand_dims(dist_info_vars["mean"][:, idx], axis=1)
+            log_stds = tf.expand_dims(dist_info_vars["log_std"][:, idx], axis=1)
+            self.means = means
+            self.log_stds = log_stds
         else:
-            zs = (x_var - means[:, idx]) / tf.exp(log_stds[:, idx])
+            # FIXME(cathywu) remove and fix this on the outside
+            x_var = tf.expand_dims(x_var, axis=1)
+            means = dist_info_vars["mean"]
+            log_stds = dist_info_vars["log_std"]
+        zs = (x_var - means) / tf.exp(log_stds)
         return - tf.reduce_sum(log_stds, axis=-1) - \
                0.5 * tf.reduce_sum(tf.square(zs), axis=-1) - \
                0.5 * self.dim * np.log(2 * np.pi)